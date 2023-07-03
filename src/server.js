bc69cf421bd722ce934d687b697a15c28a5f68ed
Andres Santillan <andres_as@live.com.ar>
2023-07-03 13:37:47 -0300

    2. Creada API RESTfull

--- src/server.js
+++ src/server.js
@@ -2,6 +2,34 @@ const express = require("express");
 const server = express();
 const PORT = 3002;
 
+server.get('/laptops', (req, res) => {
+    res.status(200).send("Laptops");
+});
+
+
+server.get('/laptops/:id', (req, res) => {
+    const { id } = req.params;
+    res.status(200).send(`Laptop ${id}`);
+});
+
+
+server.post('/laptops', (req, res) => {
+    res.status(201).send(`Laptop creada`);
+});
+
+
+server.put('/laptops/:id', (req, res) => {
+    const { id } = req.params;
+    res.status(200).send(`Datos modificados en laptop ${id}`);
+});
+
+
+server.delete('/laptops/:id', (req, res) => {
+    const { id } = req.params;
+    res.status(200).send(`Laptop ${id} eliminada`);
+
+});
+
 server.listen(PORT, () => {
     console.log(`Ejecutandose en http://localhost:${PORT}`);
 });
